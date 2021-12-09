f5084a17ba627dc2b4d14d0f52ab12dd2128b782
Eliza Weisman <eliza@buoyant.io>
2021-12-09 09:55:01 -0800

    okay this basically seems to work
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- src/util/wait/wait_queue2.rs
+++ src/util/wait/wait_queue2.rs
@@ -1,5 +1,8 @@
 use crate::{
-    loom::atomic::{AtomicBool, AtomicUsize, Ordering::*},
+    loom::{
+        atomic::{AtomicBool, AtomicUsize, Ordering::*},
+        cell::UnsafeCell,
+    },
     util::{
         mutex::Mutex,
         wait::{Notify, WaitResult},
@@ -15,11 +18,17 @@ pub(crate) struct WaitQueue<T> {
     list: Mutex<List<T>>,
 }
 
-#[pin_project::pin_project]
 #[derive(Debug)]
 pub(crate) struct Waiter<T> {
-    next: Option<NonNull<Self>>,
-    prev: Option<NonNull<Self>>,
+    node: UnsafeCell<Node<T>>,
+    queued: AtomicBool,
+}
+
+#[derive(Debug)]
+#[pin_project::pin_project]
+struct Node<T> {
+    next: Link<Waiter<T>>,
+    prev: Link<Waiter<T>>,
     waiter: Option<T>,
 
     // This type is !Unpin due to the heuristic from:
@@ -28,9 +37,11 @@ pub(crate) struct Waiter<T> {
     _pin: PhantomPinned,
 }
 
+type Link<T> = Option<NonNull<T>>;
+
 struct List<T> {
-    head: Option<NonNull<Waiter<T>>>,
-    tail: Option<NonNull<Waiter<T>>>,
+    head: Link<Waiter<T>>,
+    tail: Link<Waiter<T>>,
 }
 
 const CLOSED: usize = 1 << 0;
@@ -49,15 +60,16 @@ impl<T: Notify + Unpin> WaitQueue<T> {
 
     pub(crate) fn push_waiter(
         &self,
-        node: &mut Option<Pin<&mut Waiter<T>>>,
+        waiter: &mut Option<Pin<&mut Waiter<T>>>,
         mk_waiter: impl FnOnce() -> T,
     ) -> WaitResult {
+        test_println!("WaitQueue::push_waiter()");
         let mut state = test_dbg!(self.state.load(Acquire));
         if test_dbg!(state & CLOSED != 0) {
             return WaitResult::TxClosed;
         }
 
-        if test_dbg!(node.is_some()) {
+        if test_dbg!(waiter.is_some()) {
             while test_dbg!(state > CLOSED) {
                 match test_dbg!(self.state.compare_exchange_weak(
                     state,
@@ -83,10 +95,17 @@ impl<T: Notify + Unpin> WaitQueue<T> {
                 return WaitResult::Notified;
             }
 
-            if let Some(mut node) = node.take() {
-                *node.as_mut().project().waiter = Some(mk_waiter());
-                test_println!("-> push {:?}", node);
-                list.push_front(node);
+            if let Some(mut waiter) = waiter.take() {
+                test_println!("WaitQueue::push_waiter -> pushing {:p}", waiter);
+                if test_dbg!(waiter.queued.swap(true, Relaxed)) {
+                    return WaitResult::Wait;
+                }
+                waiter.as_mut().node.with_mut(|node| unsafe {
+                    let node = &mut *node;
+                    node.waiter = Some(mk_waiter());
+                    test_println!("-> push {:?}", node);
+                });
+                list.push_front(waiter);
             } else {
                 unreachable!("this could be unchecked...")
             }
@@ -96,6 +115,7 @@ impl<T: Notify + Unpin> WaitQueue<T> {
     }
 
     pub(crate) fn notify(&self) -> bool {
+        test_println!("WaitQueue::notify()");
         if let Some(node) = test_dbg!(self.list.lock().pop_back()) {
             node.notify();
             true
@@ -106,6 +126,7 @@ impl<T: Notify + Unpin> WaitQueue<T> {
     }
 
     pub(crate) fn close(&self) {
+        test_println!("WaitQueue::close()");
         test_dbg!(self.state.fetch_or(CLOSED, Release));
         let mut list = self.list.lock();
         while let Some(node) = list.pop_back() {
@@ -119,40 +140,73 @@ impl<T: Notify + Unpin> WaitQueue<T> {
 impl<T: Notify> Waiter<T> {
     pub(crate) fn new() -> Self {
         Self {
-            next: None,
-            prev: None,
-            waiter: None,
-            _pin: PhantomPinned,
+            node: UnsafeCell::new(Node {
+                next: None,
+                prev: None,
+                waiter: None,
+                _pin: PhantomPinned,
+            }),
+            queued: AtomicBool::new(false),
         }
     }
 
-    pub(crate) fn register(&mut self, mk_waiter: impl FnOnce() -> T) {
-        if self.waiter.is_none() {
-            self.waiter = Some(mk_waiter())
-        }
-    }
-
-    pub(crate) fn register_pinned(self: Pin<&mut Self>, mk_waiter: impl FnOnce() -> T) {
-        let this = self.project();
-        if this.waiter.is_none() {
-            *this.waiter = Some(mk_waiter())
+    pub(crate) fn register(&self, mk_waiter: impl FnOnce() -> T) {
+        unsafe {
+            self.with_node(|node| {
+                if node.waiter.is_none() {
+                    node.waiter = Some(mk_waiter())
+                }
+            })
         }
     }
 
     fn notify(self: Pin<&mut Self>) -> bool {
-        if let Some(waker) = self.project().waiter.take() {
-            waker.notify();
-            true
-        } else {
-            false
+        unsafe {
+            self.with_node(|node| {
+                if let Some(waker) = node.waiter.take() {
+                    waker.notify();
+                    true
+                } else {
+                    false
+                }
+            })
         }
+    }
+}
+
+impl<T> Waiter<T> {
+    unsafe fn with_node<U>(&self, f: impl FnOnce(&mut Node<T>) -> U) -> U {
+        self.node.with_mut(|node| f(&mut *node))
+    }
+
+    unsafe fn set_prev(&mut self, prev: Option<NonNull<Waiter<T>>>) {
+        self.node.with_mut(|node| (*node).prev = prev);
+    }
+
+    // unsafe fn set_next(&mut self, next: Option<NonNull<Waiter<T>>>) {
+    //     self.node.with_mut(|node| (*node).next = next);
+    // }
+
+    unsafe fn take_prev(&mut self) -> Option<NonNull<Waiter<T>>> {
+        self.node.with_mut(|node| (*node).prev.take())
+    }
+
+    unsafe fn take_next(&mut self) -> Option<NonNull<Waiter<T>>> {
+        self.node.with_mut(|node| (*node).next.take())
     }
 }
 
 impl<T: Notify> Waiter<T> {
     pub(crate) fn remove(self: Pin<&mut Self>, q: &WaitQueue<T>) {
-        test_println!("removing {:?}", self);
+        test_println!("Waiter::remove({:p})", self);
+
         let mut list = q.list.lock();
+
+        if !test_dbg!(self.queued.swap(false, Relaxed)) {
+            test_println!("-> the node was not queued");
+            return;
+        }
+
         unsafe {
             list.remove(self);
         }
@@ -177,17 +231,20 @@ impl<T> List<T> {
         test_dbg!(self.head.is_none())
     }
 
-    fn push_front(&mut self, node: Pin<&mut Waiter<T>>) {
-        let mut node = unsafe { node.get_unchecked_mut() };
-        node.next = self.head;
-        node.prev = None;
+    fn push_front(&mut self, waiter: Pin<&mut Waiter<T>>) {
+        unsafe {
+            waiter.with_node(|node| {
+                node.next = self.head;
+                node.prev = None;
+            })
+        }
 
-        let ptr = NonNull::from(node);
+        let ptr = unsafe { NonNull::from(Pin::into_inner_unchecked(waiter)) };
 
         assert_ne!(self.head, Some(ptr), "tried to push the same waiter twice!");
         if let Some(mut head) = self.head {
             unsafe {
-                head.as_mut().prev = Some(ptr);
+                head.as_mut().set_prev(Some(ptr));
             }
         }
 
@@ -198,40 +255,49 @@ impl<T> List<T> {
     }
 
     fn pop_back(&mut self) -> Option<Pin<&mut Waiter<T>>> {
-        let last = unsafe { self.tail?.as_mut() };
+        let mut last = self.tail?;
+        test_println!("List::pop_back() -> {:p}", last);
 
-        let prev = last.prev.take();
-        if let Some(mut prev) = prev {
-            unsafe {
-                prev.as_mut().next = None;
+        unsafe {
+            let last = last.as_mut();
+
+            let _was_queued = test_dbg!(last.queued.swap(false, Relaxed));
+            debug_assert!(_was_queued, "should have been queued!");
+            let prev = last.take_prev();
+
+            if let Some(mut prev) = prev {
+                prev.as_mut().take_next();
+            } else {
+                self.head = None;
             }
-        } else {
-            self.head = None;
+
+            self.tail = prev;
+            last.take_next();
+
+            Some(Pin::new_unchecked(last))
         }
-        self.tail = prev;
-        last.next = None;
-        unsafe { Some(Pin::new_unchecked(last)) }
     }
 
     unsafe fn remove(&mut self, node: Pin<&mut Waiter<T>>) {
         let node_ref = node.get_unchecked_mut();
-        let prev = node_ref.prev.take();
-        let next = node_ref.next.take();
+        let prev = node_ref.take_prev();
+        let next = node_ref.take_next();
         let ptr = NonNull::from(node_ref);
 
         if let Some(mut prev) = prev {
-            let prev = prev.as_mut();
-            debug_assert_eq!(prev.next, Some(ptr));
-            prev.next = next;
+            prev.as_mut().with_node(|prev| {
+                debug_assert_eq!(prev.next, Some(ptr));
+                prev.next = next;
+            })
         } else if self.head == Some(ptr) {
             self.head = next;
         }
 
         if let Some(mut next) = next {
-            let next = next.as_mut();
-            debug_assert_eq!(next.prev, Some(ptr));
-
-            next.prev = prev;
+            next.as_mut().with_node(|next| {
+                debug_assert_eq!(next.prev, Some(ptr));
+                next.prev = prev;
+            });
         } else if self.tail == Some(ptr) {
             self.tail = prev;
         };
