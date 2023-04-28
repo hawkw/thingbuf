979ed6e8d5ddc11e3d378e0a2829272d1c329624
Utkarsh Gupta <utkarshgupta137@gmail.com>
2023-04-28 22:54:27 +0530

    feat(blocking::mpsc): add `Sender::send(_ref)_timeout` methods (#79)
    Follow up to #75.
    
    Co-authored-by: Eliza Weisman <eliza@buoyant.io>

--- src/mpsc/errors.rs
+++ src/mpsc/errors.rs
@@ -1,6 +1,28 @@
 //! Errors returned by channels.
 use core::fmt;
 
+/// Error returned by the [`Sender::send_timeout`] or [`Sender::send_ref_timeout`]
+/// (and [`StaticSender::send_timeout`]/[`StaticSender::send_ref_timeout`]) methods
+/// (blocking only).
+///
+/// [`Sender::send_timeout`]: super::blocking::Sender::send_timeout
+/// [`Sender::send_ref_timeout`]: super::blocking::Sender::send_ref_timeout
+/// [`StaticSender::send_timeout`]: super::blocking::StaticSender::send_timeout
+/// [`StaticSender::send_ref_timeout`]: super::blocking::StaticSender::send_ref_timeout
+#[cfg(feature = "std")]
+#[non_exhaustive]
+#[derive(PartialEq, Eq)]
+pub enum SendTimeoutError<T = ()> {
+    /// The data could not be sent on the channel because the channel is
+    /// currently full and sending would require waiting for capacity.
+    Timeout(T),
+    /// The data could not be sent because the [`Receiver`] half of the channel
+    /// has been dropped.
+    ///
+    /// [`Receiver`]: super::Receiver
+    Closed(T),
+}
+
 /// Error returned by the [`Sender::try_send`] or [`Sender::try_send_ref`] (and
 /// [`StaticSender::try_send`]/[`StaticSender::try_send_ref`]) methods.
 ///
@@ -87,6 +109,75 @@ impl<T> fmt::Display for Closed<T> {
 
 #[cfg(feature = "std")]
 impl<T> std::error::Error for Closed<T> {}
+
+// === impl SendTimeoutError ===
+
+#[cfg(feature = "std")]
+impl SendTimeoutError {
+    pub(crate) fn with_value<T>(self, value: T) -> SendTimeoutError<T> {
+        match self {
+            Self::Timeout(()) => SendTimeoutError::Timeout(value),
+            Self::Closed(()) => SendTimeoutError::Closed(value),
+        }
+    }
+}
+
+#[cfg(feature = "std")]
+impl<T> SendTimeoutError<T> {
+    /// Returns `true` if this error was returned because the channel is still
+    /// full after the timeout has elapsed.
+    pub fn is_timeout(&self) -> bool {
+        matches!(self, Self::Timeout(_))
+    }
+
+    /// Returns `true` if this error was returned because the channel has closed
+    /// (e.g. the [`Receiver`] end has been dropped).
+    ///
+    /// If this returns `true`, no future [`try_send`] or [`send`] operation on
+    /// this channel will succeed.
+    ///
+    /// [`Receiver`]: super::blocking::Receiver
+    /// [`try_send`]: super::blocking::Sender::try_send
+    /// [`send`]: super::blocking::Sender::send
+    /// [`Receiver`]: super::blocking::Receiver
+    pub fn is_closed(&self) -> bool {
+        matches!(self, Self::Timeout(_))
+    }
+
+    /// Unwraps the inner `T` value held by this error.
+    ///
+    /// This method allows recovering the original message when sending to a
+    /// channel has failed.
+    pub fn into_inner(self) -> T {
+        match self {
+            Self::Timeout(val) => val,
+            Self::Closed(val) => val,
+        }
+    }
+}
+
+#[cfg(feature = "std")]
+impl<T> fmt::Debug for SendTimeoutError<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str(match self {
+            Self::Timeout(_) => "SendTimeoutError::Timeout(..)",
+            Self::Closed(_) => "SendTimeoutError::Closed(..)",
+        })
+    }
+}
+
+#[cfg(feature = "std")]
+impl<T> fmt::Display for SendTimeoutError<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str(match self {
+            Self::Timeout(_) => "timed out waiting for channel capacity",
+            Self::Closed(_) => "channel closed",
+        })
+    }
+}
+
+#[cfg(feature = "std")]
+impl<T> std::error::Error for SendTimeoutError<T> {}
 
 // === impl TrySendError ===
 
